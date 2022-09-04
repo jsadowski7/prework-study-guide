e9aca34f0a04287c175ae2e716e9ecf2ac102d6b
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-09-04 18:17:06 -0500

    <added for loop in console of webpage>

---
+++ Prework-Study-Guide1/assets/script.js
@@ -0,0 +1,25 @@
+//console.log = print
+// var kework for variable
+// ex. var shape = "circle";
+// variable is topics, which contains string that include all topics studied in the module
+// log method will output whatever is added in parentheses to the console
+// value is considered falsy if value is: 0, -0, On, "", null, undefined, NaN, or false.
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
+
+/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
